07d6369a020bb687fb74cfae15efcaf0e6b08af7
28kayak <kaya_ota@msn.com>
2014-07-16 15:57:19 -0700

    add successResult

--- hi.rb
+++ hi.rb
@@ -6,5 +6,16 @@ end
 post '/namereceive' do 
 	@name = params[:name]
 	@email = params[:email]
-	erb :index
+	@error_hash = {}
+	if @name.nil? or @name == ''
+		@error_hash[:name] = "name is empty"
+	elsif @email.nil? or @email = '' 
+		@error_hash[:email] = "email is empty"
+	end
+	if @error_hash.keys.length == 0
+		erb :success
+	else 
+		erb :index 
+	end
 end
+
